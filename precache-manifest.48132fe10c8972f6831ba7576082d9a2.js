6da440f950b130370ef576b3ac75f03ddd72e9d8
RahulPoddar1602 <rahul1999p@gmail.com>
2022-12-28 20:05:23 +0530

    Updates

---
+++ precache-manifest.48132fe10c8972f6831ba7576082d9a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a14af4ac524eb231a3e7608e7d02ea2",
+    "url": "/CodePen/index.html"
+  },
+  {
+    "revision": "262a54ed70881606fd2f",
+    "url": "/CodePen/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "56301ae2d55dc3b4114e",
+    "url": "/CodePen/static/css/main.18018277.chunk.css"
+  },
+  {
+    "revision": "262a54ed70881606fd2f",
+    "url": "/CodePen/static/js/2.6fb62fea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CodePen/static/js/2.6fb62fea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56301ae2d55dc3b4114e",
+    "url": "/CodePen/static/js/main.b08dd127.chunk.js"
+  },
+  {
+    "revision": "6909967701eeb7ea058c",
+    "url": "/CodePen/static/js/runtime-main.8c2db400.js"
+  }
+]);
